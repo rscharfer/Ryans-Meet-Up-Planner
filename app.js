35b9196cdae0c2b729d732548e9f68b0192f9c96
Ryan <rscharfer@gmail.com>
2016-09-10 15:48:43 +0200

    added app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var eventInput = document.querySelector('#eventInput');
+var typeEventInput = document.querySelector('#typeEventInput');
+var hostInput = document.querySelector('#hostInput');
+var startTimeInput = document.querySelector('#startTimeInput');
+var endTimeInput = document.querySelector('#endTimeInput');
+var guestInput = document.querySelector('#guestInput');
+var locationInput = document.querySelector('#locationInput');
+
+
+
